5d98505879fdf36f2883cbb359c757d339cbd32c
wangweijun1 <wangweijun1@le.com>
2017-06-29 10:53:06 +0800

    initialized project

---
+++ app/src/main/java/com/le/www/tt/MainActivity.java
@@ -0,0 +1,38 @@
+package com.le.www.tt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        int i = 0;
+//        int j = 2;
+//        long k = 2L;
+//        if (k == 2) {
+//
+//        } else {
+//
+//        }
+//        for (int i = 0; i < 5; i++) {
+//
+//        }
+//
+//        StringBuffer sb = new StringBuffer();
+//超过120个字符的情况下，换行缩进4个空格，并且方法前的点符号一起换行
+//        sb.append("zi")
+//                .append("xin")
+//                .append("huang")
+//                .append("huang")
+//                .append("huang");
+
+        int i = -1;
+//        Integer integer = null;
+//        i = integer;
+//        Log.i("wang", "i="+i);
+    }
+}
